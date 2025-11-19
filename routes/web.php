2e1f46119ad29febe5cc4615b7c9e319ce0f8b36
fvnhndito <ditohananto@gmail.com>
2025-11-19 22:58:19 +0700

    membuat tampilan register

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
 });
 
 Route::get('/login', function () {
     return view('login');
 })->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
